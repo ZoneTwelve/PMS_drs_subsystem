22f0a587328b72d284d7ffca88cfd9222abb604b
ZoneTwelve <zonetwelve159@gmail.com>
2021-07-28 00:53:02 +0800

    [update] genreate qr code

--- public/src/js/shsd/drs_gen_qrcode.js
+++ public/src/js/shsd/drs_gen_qrcode.js
@@ -4,11 +4,45 @@ window.onload = function(){
     width : 1024,
     height: 1024
   });
-  makeQRCode(`${location.origin}/SampleHere`);
+  document.querySelector("#generate").onclick = makeQRCode;
+  makeQRCode(`Sample Here`);
+  fetch("/api/v1/shsd/drs_group", {
+    method:"GET",
+  }).then( res => {
+    if( res.status == 401 ){
+      alert("出事啦");
+      return false;
+    }
+
+    res.json().then( applyDataSelector )
+  } );
 }
 
+function applyDataSelector( data ){
+  let form = document.qr_form;
+
+  form.group_id.innerHTML = "";
+  form.group_id.onchange = ( ) => {
+
+  }
+  form.group_id.appendChild(createElement("option", { hidden:true, innerText: `請選擇項目`}));
+  for( let d of data ){
+    form.group_id.appendChild(createElement("option", { 
+      value: d.dgs_id, innerText: `(${d.visible?"可選":"隱藏"}) ${d.dgs_id} - ${d.name}`,
+      data_title: d.name
+    }));
+  }
+  console.log( data );
+}
 
 function makeQRCode( text ){
-  qrcode.makeCode( text );
-  document.querySelector("#content").innerText = text;
+  let form = document.qr_form;
+  let data = {
+    title: (form.group_id.options[ form.group_id.selectedIndex ] || {innerText:"sample"}).data_title,
+    value: form.group_id.value,
+  };
+  console.log( data );
+  let buf = btoa(unescape(encodeURIComponent(JSON.stringify( data ))));
+  qrcode.makeCode( buf );
+  // document.querySelector("#content").innerText = text;
 }
